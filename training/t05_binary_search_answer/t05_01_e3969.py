4114b99940070253f5b4e4ac88ab345347e88d7d
OlegTyshchenko <sokol.wise@gmail.com>
2026-01-20 18:33:39 +0200

    solved t05_01_e3969

---
+++ training/t05_binary_search_answer/t05_01_e3969.py
@@ -0,0 +1,17 @@
+def binary_search(w,h,n):
+    low = 1
+    high = n*max(w,h)
+    while low < high:
+        a = low + (high - low) // 2
+        count = (a//w) * (a//h)
+        if count < n:
+            low = a + 1
+        else:
+            high = a
+
+    return low
+
+
+if __name__ == "__main__":
+    w,h,n = map(int,input().split())
+    print(binary_search(w,h,n))
