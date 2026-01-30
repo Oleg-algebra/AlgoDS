4d81605e9f2a00dee816976bf3e81a3129ea60ea
OlegTyshchenko <sokol.wise@gmail.com>
2026-01-30 14:21:42 +0200

    t02-time-complexity-solutions-2

---
+++ t02_time_complexity/t02_06.py
@@ -0,0 +1,12 @@
+def sum_arithm(n):
+    return n*(n+1)/2    #O(1)
+
+def sum_geom(a,n):
+    if a = 1:
+        return n + 1        # O(1)
+    else:
+        return (a**n - 1)/(a-1)     # O(log n)
+
+
+def sum_geom2(a):
+    return 1/(1-a)          # O(1)
