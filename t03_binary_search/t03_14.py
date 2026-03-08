2d4d0abe21b8c8c8096883be149b2687fd580dc3
OlegTyshchenko <sokol.wise@gmail.com>
2026-03-08 19:44:49 +0200

    moved files

--- t03_binary_search/t03_14.py
+++ t03_binary_search/t03_14.py
@@ -7,7 +7,7 @@ def binary_search_right(array,x):
     left = 0
     right = len(array)
 
-    while left <= right:
+    while left < right:
         mid  = left + (right - left) // 2
         print(f'left: {left}, right: {right}, mid: {mid}')
         # mid = (left + right) // 2
@@ -20,22 +20,24 @@ def binary_search_right(array,x):
     return left - 1
 
 if __name__ == "__main__":
-    n,m = list(map(int,input().split()))
-    array = list(map(int,input().split()))
-    request_x = []
-    for i in range(m):
-        x = int(input())
-        request_x.append(x)
-
-    for x in request_x:
-        res = binary_search_right(array,x)
-        if array[res] == x:
-            print(res + 1)
-        else:
-            print(0)
-    # array = [2,3,3,3,5,6,7,8,8,8,9,9]
-    # print(len(array))
-    # print(binary_search_right(array,3))
-    # print(binary_search_right(array, 4))
-    # print(binary_search_right(array, 10))
-    # print(binary_search_right(array, 0))
+    # n,m = list(map(int,input().split()))
+    # array = list(map(int,input().split()))
+    # request_x = []
+    # for i in range(m):
+    #     x = int(input())
+    #     request_x.append(x)
+    #
+    # for x in request_x:
+    #     res = binary_search_right(array,x)
+    #     if array[res] == x:
+    #         print(res + 1)
+    #     else:
+    #         print(0)
+    array = [2,3,3,3,5,6,7,8,8,8,9,9]
+    # array = array[::-1]
+    print(array)
+    print(len(array))
+    print(binary_search_right(array,3))
+    print(binary_search_right(array, 4))
+    print(binary_search_right(array, 10))
+    print(binary_search_right(array, 0))
