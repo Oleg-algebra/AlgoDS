154d41685e887505f7e9b62625e65e2b8cfec581
OlegTyshchenko <sokol.wise@gmail.com>
2026-03-09 10:51:52 +0200

    t10_brute_force code solutions

---
+++ t10_brute_force/t10_02_e1296.py
@@ -0,0 +1,28 @@
+
+max_value: int
+
+def solve(nums: str, pieces: int, value: int):
+    global max_value
+
+    product = int(nums) * value
+    if max_value > product:
+        return
+    elif pieces == 1:
+        max_value = product
+        return
+
+    for i in range(1,len(nums) - pieces + 2):
+        sub_nums = nums[i:]
+        sub_value = value * int(nums[:i])
+        solve(sub_nums,pieces - 1,sub_value)
+
+if __name__ == "__main__":
+
+    f = open("input.txt")
+    for line in f:
+        n,m = line.split()
+        max_value = 0
+        solve(n,int(m),1)
+        print(max_value)
+
+    f.close()
