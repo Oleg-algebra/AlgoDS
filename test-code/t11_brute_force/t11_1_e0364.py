cad0de8c46541d06ac3891e3ef00c1656fd24c8b
OlegTyshchenko <sokol.wise@gmail.com>
2026-03-12 22:39:11 +0200

    t11_brute_force test code

---
+++ test-code/t11_brute_force/t11_1_e0364.py
@@ -0,0 +1,30 @@
+from math import factorial
+
+alphabet: list[str]
+N: int
+k: int
+count: int = 0
+
+
+def solve(word):
+    global count
+
+    if len(word) == N:
+        count  += 1
+        # print(count,word)
+        return word
+
+    for letter in alphabet:
+        if letter not in word:
+            res = solve(word + letter)
+            if count == k:
+                return res
+
+
+
+if __name__ == "__main__":
+    N,k = map(int,input().split())
+
+    alphabet = [chr(ord("a") + i) for i in range(N)]
+    # print(alphabet)
+    print(solve(""))
