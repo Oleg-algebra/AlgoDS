84906f8d71db12cd1ad3a674d41e0fb88ca9fa7a
OlegTyshchenko <sokol.wise@gmail.com>
2026-05-11 10:35:46 +0300

    t24_maze solution code

---
+++ t24_maze/t24_01_e4001_V2.py
@@ -0,0 +1,85 @@
+import os
+import time
+from collections import deque
+
+# Константи для стану клітинок
+EMPTY = 0
+VISITED = 1
+
+# Символи для відображення в консолі
+WALL_CHAR = "*"  # Стіна
+CELL_CHAR = "·"  # Недосліджена клітинка
+VISITED_CHAR = "▒"  # Досліджена клітинка
+CURRENT_CHAR = "@"  # Поточна клітинка в обробці
+
+
+class Maze:
+    def __init__(self, maze):
+        self.maze = maze
+        self.n = len(maze)
+        self.directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+    def _clear_console(self):
+        # Очищення консолі для Windows (cls) та Linux/Mac (clear)
+        os.system('clear')
+
+    def _draw(self, wave_matrix, current_pos):
+        """Метод для малювання поточного стану лабіринту"""
+        self._clear_console()
+        output = []
+        for i in range(self.n):
+            row = []
+            for j in range(len(self.maze[i])):
+                if (i, j) == current_pos:
+                    row.append(CURRENT_CHAR)
+                elif self.maze[i][j] == "*":
+                    row.append(WALL_CHAR)
+                elif wave_matrix[i][j] == VISITED:
+                    row.append(VISITED_CHAR)
+                else:
+                    row.append(CELL_CHAR)
+            output.append(" ".join(row))
+
+        print("\n".join(output))
+        print(f"\nExplored cells: {sum(row.count(VISITED) for row in wave_matrix)}")
+        time.sleep(1)  # Затримка в 0.1 секунди (можна змінити)
+
+    def dfs_count_area(self, si, sj):
+        wave_matrix = [[EMPTY] * self.n for _ in range(self.n)]
+        wandering = [[EMPTY] * self.n for _ in range(self.n)]
+        count = 0
+        stack = [(si, sj)]
+
+        wave_matrix[si][sj] = VISITED
+        while stack:
+            i, j = stack.pop()
+            count += 1
+
+            # Візуалізуємо кожен крок
+            # self._draw(wandering, (i, j))
+            wandering[i][j] = VISITED
+
+            for di, dj in self.directions:
+                ni, nj = i + di, j + dj
+
+                # Перевірка меж та стану
+
+                if self.maze[ni][nj] == "." and wave_matrix[ni][nj] == EMPTY:
+                    wave_matrix[ni][nj] = VISITED
+                    stack.append((ni, nj))
+
+        return count
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        n = int(f.readline().strip())
+
+        maze_matrix = [
+            list(f.readline().strip()) for _ in range(n)
+        ]
+
+        si,sj = map(lambda x: int(x) - 1, f.readline().split())
+
+        maze = Maze(maze_matrix)
+        print(maze.dfs_count_area(si, sj))
